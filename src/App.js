5fe5e52c05dc9a129bf778363e512c10ced2a9b4
Sam Esgate <sam.esgate@sparx.co.uk>
2021-05-17 22:03:28 +0100

    Animate you

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {useEffect, useRef} from "react";
+import anime from "animejs";
 
 const unit = 4;
 const canvasWidth = 1280;
@@ -8,19 +9,24 @@ const gapToBottom = unit * 10;
 const gapToRight = gapToBottom;
 const timelineWidth = canvasWidth - gapToRight;
 const timelineHeight = unit * 4;
+const youRadius = unit * 6;
 
-function drawYou(ctx) {
-    const youRadius = unit * 6;
-    const strokeColour = "red";
-    const fillColour = "pink";
-    const x = canvasWidth - gapToRight - youRadius / 2;
-    const y = canvasHeight - gapToBottom - timelineHeight / 2;
-    ctx.beginPath();
-    ctx.strokeStyle = strokeColour;
-    ctx.arc(x, y, youRadius, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fillStyle = fillColour;
-    ctx.fill();
+function blankCanvas(ctx) {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+}
+
+function drawPixel(ctx, x, y, colour) {
+    ctx.fillStyle = colour;
+    ctx.fillRect(x, y, 1, 1);
+}
+
+function drawPixels(ctx) {
+    for (let i = 0; i < 300; i++) {
+        const x = Math.floor(Math.random() * canvasWidth);
+        const y = Math.floor(Math.random() * canvasHeight);
+        const lightness = Math.floor(Math.random() * 255);
+        drawPixel(ctx, x, y, `rgb(${lightness} ${lightness} ${lightness}`);
+    }
 }
 
 function drawTimeline(ctx) {
@@ -35,18 +41,48 @@ function drawTimeline(ctx) {
     ctx.fill();
 }
 
-function drawPixel(ctx, x, y, colour) {
-    ctx.fillStyle = colour;
-    ctx.fillRect(x, y, 1, 1);
+function drawYou(ctx, ballPosition) {
+    const strokeColour = "red";
+    const fillColour = "pink";
+    ctx.beginPath();
+    ctx.strokeStyle = strokeColour;
+    ctx.arc(ballPosition.x, ballPosition.y, youRadius, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fillStyle = fillColour;
+    ctx.fill();
 }
 
-function drawPixels(ctx) {
-    for (let i = 0; i < 1000; i++) {
-        const x = Math.floor(Math.random() * canvasWidth);
-        const y = Math.floor(Math.random() * canvasHeight);
-        const lightness = Math.floor(Math.random() * 255);
-        drawPixel(ctx, x, y, `rgb(${lightness} ${lightness} ${lightness}`);
+function animateYou(ctx) {
+    let ballPosition = {
+        x: canvasWidth / 2,
+        y: canvasHeight / 2,
     }
+
+    function render() {
+        blankCanvas(ctx);
+        drawPixels(ctx);
+        drawTimeline(ctx);
+        drawYou(ctx, ballPosition);
+    }
+
+    anime.timeline({
+        targets: ballPosition,
+        loop: true,
+        update: render,
+        duration: 2000,
+    }).add({
+        keyframes: [
+            {
+                x: canvasWidth / 2,
+                y: canvasHeight / 2,
+            },
+            {
+                x: canvasWidth - gapToRight - youRadius / 2,
+                y: canvasHeight - gapToBottom - timelineHeight / 2,
+            },
+        ],
+        easing: 'cubicBezier(0.450, 0.010, 0.010, 1.000)',
+    });
 }
 
 function App() {
@@ -60,10 +96,7 @@ function App() {
         canvasRef.current.style.height = canvasHeight + 'px';
         const ctx = canvasRef.current.getContext('2d');
 
-        drawPixels(ctx);
-        drawYou(ctx);
-        drawTimeline(ctx);
-
+        animateYou(ctx);
     }, [canvasRef])
 
     return (
